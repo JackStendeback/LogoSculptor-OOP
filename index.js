943ff2c4393a0f0a7c90a8cf17353d6fd8964a90
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-08-23 23:01:00 -0500

    finished inquirer prompts, trying to create an svg file upon completion

--- index.js
+++ index.js
@@ -8,16 +8,33 @@ inquirer.prompt([
         message: 'Enter up to three characters'
     },
     {
-        type: 'list',
+        type: 'input',
         name: 'svgColor',
         message:  'Please enter a color'
     },
     {
-
-
-        
+        type: 'list',
+        name: 'svgShape',
+        message: 'Please choose a shape',
+        choices: ['square', 'circle', 'triangle']
+    },
+    {
+        type: 'input',
+        name: 'svgShapeColor',
+        message: 'Please enter your preferred shape color'
     }
-])
+]).then(answers => {
+    const svgContent = answers.svgContent;
+    const generatedSVG = `<svg>${svgContent}</svg>`;
+
+    fs.writeFile('output.svg', generatedSVG, 'utf8', (err) => {
+        if (err) {
+          console.error('Error writing SVG file:', err);
+        } else {
+          console.log('SVG file generated successfully.');
+        }
+      });
+    });
 
 
 
