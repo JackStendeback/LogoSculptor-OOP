7a5e0c86688178ebb49e75dd496addcc4073f060
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-08-24 17:17:08 -0500

    restructuring code into functions... going to test svg now

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'svgContent',
-        message: 'Enter up to three characters',
-        validate: function(input) {
-            if (input.length > 3) {
-                return "Please enter up to three characters.";
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'svgColor',
-        message:  'Please enter a color or hexadecimal value',
-        validate: function(input) {
-            if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input) || /^[a-zA-Z]+$/.test(input)) {
+async function getUserInput() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'svgContent',
+            message: 'Enter up to three characters',
+            validate: function(input) {
+                if (input.length > 3) {
+                    return "Please enter up to three characters.";
+                }
                 return true;
             }
-            return "Please enter a valid color keyword or hexadecimal value.";
-        }
-    },
-    {
-        type: 'list',
-        name: 'svgShape',
-        message: 'Please choose a shape',
-        choices: ['square', 'circle', 'triangle']
-    },
-    {
-        type: 'input',
-        name: 'svgShapeColor',
-        message: 'Please enter your preferred shape color',
-        validate: function(input) {
-            if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input) || /^[a-zA-Z]+$/.test(input)) {
-                return true;
+        },
+        {
+            type: 'input',
+            name: 'svgColor',
+            message:  'Please enter a color or hexadecimal value',
+            validate: function(input) {
+                if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input) || /^[a-zA-Z]+$/.test(input)) {
+                    return true;
+                }
+                return "Please enter a valid color keyword or hexadecimal value.";
             }
-            return "Please enter a valid color keyword or hexadecimal value.";
+        },
+        {
+            type: 'list',
+            name: 'svgShape',
+            message: 'Please choose a shape',
+            choices: ['square', 'circle', 'triangle']
+        },
+        {
+            type: 'input',
+            name: 'svgShapeColor',
+            message: 'Please enter your preferred shape color',
+            validate: function(input) {
+                if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input) || /^[a-zA-Z]+$/.test(input)) {
+                    return true;
+                }
+                return "Please enter a valid color keyword or hexadecimal value.";
+            }
         }
-    }
-]).then(answers => {
+    ]);
+}
+
+getUserInput().then(answers => {
     const svgContent = answers.svgContent.substring(0, 3);
     const svgColor = answers.svgColor;
     const svgShape = answers.svgShape;
     const svgShapeColor = answers.svgShapeColor;
 
+    // Variables for shapeSVG, textX, and textY should be declared here
+    let shapeSVG = '';
     let textX = 0;
     let textY = 0;
 
-
-
-function generateShapeSVG(svgShape, svgShapeColor) {
-
-    let shapeSVG = '';
-    
     if (svgShape === 'circle') {
         shapeSVG = `<circle cx="150" cy="100" r="40" fill="${svgShapeColor}" />`;
         textX = 150;
@@ -69,22 +69,19 @@ function generateShapeSVG(svgShape, svgShapeColor) {
         textX = 150;
         textY = 130;
     }
-    return shapeSVG;
-}
 
     const generatedSVG = `<svg width="300" height="200">
     ${shapeSVG}
     <text x="${textX}" y="${textY}" fill="${svgColor}" text-anchor="middle">${svgContent}</text>
     </svg>`;
-    
+
     fs.writeFile('logo.svg', generatedSVG, 'utf8', (err) => {
         if (err) {
-          console.error('Error writing SVG file:', err);
+            console.error('Error writing SVG file:', err);
         } else {
-          console.log('Generated logo.svg.');
+            console.log('Generated logo.svg.');
         }
-      });
     });
-
+});
 
 
